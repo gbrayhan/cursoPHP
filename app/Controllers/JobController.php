8ae6b39deeb36af02b97e2e659b73e932231ead7
Alejandro Guerrero <gbrayhan@gmail.com>
2018-10-08 00:07:01 -0500

    Validacion de entradas en formulario y Carga de Archivos

--- app/Controllers/JobController.php
+++ app/Controllers/JobController.php
@@ -1,26 +1,47 @@
 <?php
 	namespace App\Controllers;
+	
 	use App\Models\Job;
-
+	use Respect\Validation\Validator as v;
 
 
 	class JobController extends BaseController {
 		public function getAddJobAction($request){
-			// echo "Funcion Get Add Job Action";
-			// var_dump($request->getBody());
-			// var_dump($request->getParsedBody());
-
+			$responseMessage = null;
+			
 			if ($request->getMethod() == 'POST') {
 
 				$postData = $request->getParsedBody();
 
-				$job = new Job();
-				$job->title = $postData['title'];
-				$job->description = $postData['description'];
-				$job->save();	
+				$jobValidator = v::key('title', v::stringType()->notEmpty())->key('description', v::stringType()->notEmpty());
+
+	            try {
+	                $jobValidator->assert($postData);
+	                $postData = $request->getParsedBody();
+
+	                $files = $request->getUploadedFiles();//SuperGlobal $_FILE
+	                $logo = $files['logo'];
+
+	                if($logo->getError() == UPLOAD_ERR_OK) {
+	                    $fileName = $logo->getClientFilename();
+	                    $logo->moveTo("uploads/$fileName");
+	                }
+
+	                $job = new Job();
+	                $job->title = $postData['title'];
+	                $job->description = $postData['description'];
+	                $job->logo = $fileName;
+	                $job->save();
+
+	                $responseMessage = 'Saved';
+	            } catch (\Exception $e) {
+	                $responseMessage = $e->getMessage();
+	            }
 			}
 
-			return  $this->renderHTML('addJob.twig', []);
+			return $this->renderHTML('addJob.twig', [
+            	'responseMessage' => $responseMessage
+        	]);
 		}
 	} 
 
