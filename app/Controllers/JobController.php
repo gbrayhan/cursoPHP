2799a1231be7c55c1190b3c321edeb44318ba85f
Alejandro Guerrero <gbrayhan@gmail.com>
2018-10-07 22:26:45 -0500

    Extendiendo templates con twig

--- app/Controllers/JobController.php
+++ app/Controllers/JobController.php
@@ -7,8 +7,8 @@
 	class JobController extends BaseController {
 		public function getAddJobAction($request){
 			// echo "Funcion Get Add Job Action";
-			var_dump($request->getBody());
-			var_dump($request->getParsedBody());
+			// var_dump($request->getBody());
+			// var_dump($request->getParsedBody());
 
 			if ($request->getMethod() == 'POST') {
 
@@ -19,7 +19,8 @@
 				$job->description = $postData['description'];
 				$job->save();	
 			}
-			echo $this->renderHTML('addJob.twig');
+
+			return  $this->renderHTML('addJob.twig', []);
 		}
 	} 
 
